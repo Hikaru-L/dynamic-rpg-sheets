a6563e274826f6ff6e77265767d2c3b7fd071b60
Hikaru <hikaru.outsmart@gmail.com>
2021-02-13 16:09:12 -0300

    minor fixing of files imported from another project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import styled from 'styled-components';
-import { RotatingBranch, BranchPosition} from './components/RotatingBranch';
 
 const Background = styled.div`
   display: flex;
@@ -22,16 +19,6 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
       <Background>
-        <Pivot>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={0} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={45} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={90} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={135} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={180} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={225} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={270} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-          <RotatingBranch shouldRenderInnerlines={state} initialAngle={315} layer={LAYERS} isFirst length={600} width={2} position={BranchPosition.CENTER}/>
-        </Pivot>
       </Background>
   <button style={{marginLeft: '600px'}} onClick={()=> setState(!state)}>{state ? 'ON' : 'OFF'}</button>
       </header>
